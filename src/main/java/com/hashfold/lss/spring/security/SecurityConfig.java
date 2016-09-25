0c54e845a47d6a64a568292073eeadb5633a325d
pravsingh <psinghiitr@gmail.com>
2016-09-25 02:50:00 -0700

    Basic Security: using user service & roles

--- src/main/java/com/hashfold/lss/spring/security/SecurityConfig.java
+++ src/main/java/com/hashfold/lss/spring/security/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com.hashfold.lss.spring.security;
 
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,8 +17,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+		auth.inMemoryAuthentication();// .withUser("user").password("password").roles("USER");
+		auth.userDetailsService(inMemoryUserDetailsManager());
 
 		super.configure(auth);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/delete/**").hasRole("ADMIN").anyRequest().authenticated().and()
+				.formLogin().and().httpBasic();
+	}
+
+	@Bean
+	public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+		final Properties users = new Properties();
+
+		users.put("user", "pass,ROLE_USER,enabled");
+		users.put("admin", "pass,ROLE_ADMIN,enabled");
+
+		return new InMemoryUserDetailsManager(users);
 	}
 }
